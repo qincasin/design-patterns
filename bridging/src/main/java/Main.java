0cfdd37ac75d603ff0bec08dcd2fb9fd2fb2f52f
qinjiaxing <qincasin@163.com>
2021-03-09 11:12:56 +0800

    feat: 桥接模式实现 支付方式下的 不同类型支付(指纹、密码、人脸)

--- bridging/src/main/java/Main.java
+++ bridging/src/main/java/Main.java
@@ -10,8 +10,17 @@ import java.math.BigDecimal;
  * Created by qincasin on 2021/1/16.
  */
 public class Main {
+    /**
+     * 左侧 Pay 是⼀个抽象类，往下是它的两个⽀付类型实现；微信⽀付、⽀付宝⽀付。
+     * 右侧 IPayMode 是⼀个接⼝，往下是它的两个⽀付模型；刷脸⽀付、指纹⽀付。
+     * 那么， ⽀付类型 × ⽀付模型 = 就可以得到相应的组合。
+     * 注意，每种⽀付⽅式的不同，刷脸和指纹校验逻辑也有差异，可以使⽤适配器模式进⾏处理，这⾥
+     * 不是本⽂᯿点不做介绍，可以看适配器模式章节。
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("模拟微信人脸方式");
+        //模拟查询db中的数据，如果有多个，按照顺序进行初始化
         Pay wxPay = new WxPay(new PayFaceMode());
         wxPay.transfer("weixin_1092033111","100000109893",new BigDecimal(100));
 
